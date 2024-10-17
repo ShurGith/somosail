ae5603b9778fbced2c50fb2d2ceeee497e0136fc
ShurGith <75266693+ShurGith@users.noreply.github.com>
2024-10-18 01:10:03 +0200

    Artes Mysql DB

---
+++ database/category_post.sql
@@ -0,0 +1,2 @@
+INSERT INTO `category_post` (`id`, `post_id`, `category_id`, `user_id`, `created_at`, `updated_at`) VALUES (1, 1, 1, 11, NULL, NULL);
+INSERT INTO `category_post` (`id`, `post_id`, `category_id`, `user_id`, `created_at`, `updated_at`) VALUES (2, 2, 5, 11, NULL, NULL);
